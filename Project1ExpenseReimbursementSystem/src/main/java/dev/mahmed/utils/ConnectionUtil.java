7e4909ead51d3a5bc441d596a723bfdbb6d99c9a
rizhalldev <mahmed35@nyit.edu>
2020-04-12 18:00:51 -0400

    Added MariaDB dependency, properties, and ConnectionUtil

---
+++ Project1ExpenseReimbursementSystem/src/main/java/dev/mahmed/utils/ConnectionUtil.java
@@ -0,0 +1,44 @@
+package dev.mahmed.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+private static Connection conn = null;
+	
+	public static Connection createConnection() {
+				
+		Properties props = new Properties();
+				
+		try {
+		FileInputStream in = new FileInputStream("src/main/resources/connection.properties");
+		props.load(in);
+					
+		String details = props.getProperty("condetails");
+					
+		Connection conn = DriverManager.getConnection(details);
+//		System.out.println(conn);
+
+		return conn;
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+//	public static void main(String[] args) {
+//	createConnection();
+//}
+}
