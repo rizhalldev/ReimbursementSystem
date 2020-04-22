29038c26624bf06fa956481cad2c300f23380f7c
rizhalldev <mahmed35@nyit.edu>
2020-04-22 00:28:46 -0400

    Added getEmployeeManager URI to the DispatcherServlet

--- Project1ExpenseReimbursementSystemServer/src/main/java/dev/mahmed/servlets/DispatcherServlet.java
+++ Project1ExpenseReimbursementSystemServer/src/main/java/dev/mahmed/servlets/DispatcherServlet.java
@@ -25,6 +25,7 @@ public class DispatcherServlet extends HttpServlet {
 		case "/Project1ExpenseReimbursementSystemServer/api/employeelogin": econtrol.employeeLogin(request, response); break;
 		case "/Project1ExpenseReimbursementSystemServer/api/getloggedemployee": econtrol.getLoggedEmployee(request, response); break;
 		case "/Project1ExpenseReimbursementSystemServer/api/getemployeereimbursements": econtrol.getEmployeeReimbursements(request, response); break;
+		case "/Project1ExpenseReimbursementSystemServer/api/getemployeemanager": econtrol.getEmployeeManager(request, response); break;
 		
 		default: break;
 		}
