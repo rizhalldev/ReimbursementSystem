04df2d0d0e98c4ebf5155b8a9d8733d86ceab0c7
rizhalldev <mahmed35@nyit.edu>
2020-04-25 03:07:10 -0400

    Created layout for area where employees will create reimbursement requests.

--- Project1ExpenseReimbursementSystemServer/src/main/java/dev/mahmed/servlets/DispatcherServlet.java
+++ Project1ExpenseReimbursementSystemServer/src/main/java/dev/mahmed/servlets/DispatcherServlet.java
@@ -10,29 +10,44 @@ import dev.mahmed.controllers.EmployeeController;
 
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public DispatcherServlet() {
-        super();
-    }
-    
-    EmployeeController econtrol = new EmployeeController();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public DispatcherServlet() {
+		super();
+	}
+
+	EmployeeController econtrol = new EmployeeController();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String uri = request.getRequestURI();
 		System.out.println(uri);
 		switch (uri) {
-		
-		case "/Project1ExpenseReimbursementSystemServer/api/employeelogin": econtrol.employeeLogin(request, response); break;
-		case "/Project1ExpenseReimbursementSystemServer/api/getloggedemployee": econtrol.getLoggedEmployee(request, response); break;
-		case "/Project1ExpenseReimbursementSystemServer/api/getemployeereimbursements": econtrol.getEmployeeReimbursements(request, response); break;
-		case "/Project1ExpenseReimbursementSystemServer/api/getemployeemanager": econtrol.getEmployeeManager(request, response); break;
-		case "/Project1ExpenseReimbursementSystemServer/api/getsessionreimbursements": econtrol.getSessionReimbursements(request, response); break;
-		case "/Project1ExpenseReimbursementSystemServer/api/cancelreimbursement": econtrol.cancelReimbursement(request, response); break;
-		default: break;
+
+		case "/Project1ExpenseReimbursementSystemServer/api/employeelogin":
+			econtrol.employeeLogin(request, response);
+			break;
+		case "/Project1ExpenseReimbursementSystemServer/api/getloggedemployee":
+			econtrol.getLoggedEmployee(request, response);
+			break;
+		case "/Project1ExpenseReimbursementSystemServer/api/getemployeereimbursements":
+			econtrol.getEmployeeReimbursements(request, response);
+			break;
+		case "/Project1ExpenseReimbursementSystemServer/api/getemployeemanager":
+			econtrol.getEmployeeManager(request, response);
+			break;
+		case "/Project1ExpenseReimbursementSystemServer/api/getsessionreimbursements":
+			econtrol.getSessionReimbursements(request, response);
+			break;
+		case "/Project1ExpenseReimbursementSystemServer/api/cancelreimbursement":
+			econtrol.cancelReimbursement(request, response);
+			break;
+		default:
+			break;
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
